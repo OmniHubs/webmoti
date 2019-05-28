72520a20998360a5b8aa13dfb6851772f8f0477a
imdc <garo567@hotmail.com>
2019-05-28 17:53:14 -0400

    material-ui navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import webMoti from './images/webMoti.png';
-import { Button } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap/Navbar'
+import Button from '@material-ui/core/Button';
+import Navbar from './components/Navbar'
 
 
 
@@ -28,40 +25,18 @@ import { Navbar } from 'react-bootstrap/Navbar'
     render() {
         return (
             <div className="App">
-                <header>
-                    <div>
-                        <Navbar color="light" light expand="md">
-
-                        </Navbar>;
-                    </div>
-                    );
-                    }
-                    }
-                    {/*<img src={webMoti} alt="webMoti" width="100" height="100"/>*/}
-                </header>
+                <Navbar/>
 
                 <body>
 
                 <div className="container-fluid">
                     {/*<img id="hello"  src={Background} />*/}
                     <div className="text-block">
-                        <Button variant="primary" onClick="login()">
+                        <Button variant="primary">
                             Login To your account
                         </Button>
                     </div>
                 </div>
-                {/*<Container>*/}
-                {/*    <Row>*/}
-                {/*        <Col>1 of 3</Col>*/}
-                {/*        <Col xs={6}>2 of 3 (wider)</Col>*/}
-                {/*        <Col>3 of 3</Col>*/}
-                {/*    </Row>*/}
-                {/*    <Row>*/}
-                {/*        <Col>1 of 3</Col>*/}
-                {/*        <Col xs={5}>2 of 3 (wider)</Col>*/}
-                {/*        <Col>3 of 3</Col>*/}
-                {/*    </Row>*/}
-                {/*</Container>;*/}
 
                 </body>
             </div>

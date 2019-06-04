0934e8b603ada36444812cecf3da0bc4be454074
imdc <garo567@hotmail.com>
2019-06-04 12:58:27 -0400

    front

--- routing/classroom.js
+++ routing/classroom.js
@@ -4,12 +4,7 @@
 
 var express = require("express");
 var router = express.Router();
-const peerJs = require('peerjs-nodejs');
-const peer = peerJs({ key: 'abcxyz' });
 
-let conn = peer.connect('foo');
-conn.serialization = 'json';
-conn.send({ value: 'hello' });
 
 router.get("/", function(req, res, next) {
     res.send("API is working properly");
